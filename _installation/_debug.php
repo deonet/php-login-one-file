84c2556aa85265d2b979ef950705b8938357c843
deonet <72336616+deonet@users.noreply.github.com>
2020-10-04 21:47:54 +0700

    Update _debug.php

--- _installation/_debug.php
+++ _installation/_debug.php
@@ -13,7 +13,7 @@ $db_type = "sqlite";
 $db_sqlite_path = "../users.db";
 
 // create new database connection
-$db_connection = new PDO($db_type . ':' . $db_sqlite_path);
+$db_connection = new PDO($db_type . ':' . $db_sqlite_path);if(1==2){
 
 // query
 $sql = 'SELECT * FROM users';
@@ -23,4 +23,33 @@ $query = $db_connection->prepare($sql);
 $query->execute();
 
 // show all the data from the "users" table inside the database
-var_dump($query->fetchAll());
+var_dump($query->fetchAll());}else{
+	
+	echo'<pre>';
+	$stmt=$db_connection->query('SELECT name '
+			.'FROM sqlite_master '
+			."WHERE type='table' AND name !='' "
+		);
+	$count=0;
+	while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+		$count++;
+		echo"Table: ".$row['name']."<br>";
+		// query
+		$sql = 'SELECT * FROM ' . $row['name'] ;
+		// execute query
+		$query = $db_connection->prepare($sql);
+		$query->execute();
+		// show all the data from the "users" table inside the database
+		print_r($query->fetchAll());
+	}
+	if(!$count )print_r(
+		array(
+			'info: no table',
+			'do: install',
+		)
+	);
+	echo'</pre>';
+	
+}
+
+
